ed4f4466bd43d906e070f7872e484402fc11aee6
Matt <matt.bentonn@hotmail.com>
2025-04-28 21:01:00 +1000

    removed terraform. added an nginx config and a docker file to spin up all services in one

--- terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = var.region
-
-  # These can be set via environment variables:
-  # AWS_ACCESS_KEY_ID
-  # AWS_SECRET_ACCESS_KEY
-  # AWS_SESSION_TOKEN (if using temporary credentials)
-} 
